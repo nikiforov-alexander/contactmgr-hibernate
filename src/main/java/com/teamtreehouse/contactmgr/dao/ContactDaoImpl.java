59b3b82fb03fb510f02ae493292302d58a036f4a
Alexander Nikiforov <nikiforo.alexander@gmail.com>
2017-01-29 09:37:10 +0100

    Add contructor and sessionFactory member
    to ContactDaoImpl.
    
    sessionFactory is final to make sure it is made only once, however not static ... which may not be right.
    
    Constructor takes a parameter: hibernate.cfg.xml file name. It is needed for testing purposes to apply testing database.

--- src/main/java/com/teamtreehouse/contactmgr/dao/ContactDaoImpl.java
+++ src/main/java/com/teamtreehouse/contactmgr/dao/ContactDaoImpl.java
@@ -1,10 +1,53 @@
 package com.teamtreehouse.contactmgr.dao;
 
 import com.teamtreehouse.contactmgr.model.Contact;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.service.ServiceRegistry;
 
 import java.util.List;
 
 public class ContactDaoImpl implements ContactDao {
+
+    private final SessionFactory sessionFactory;
+
+    /**
+     * build Hibernate's sessionFactory with
+     * provided in constructor {@literal hibernateConfigurationFile}
+     * see <a href="http://docs.jboss.org/hibernate/orm/5.2/userguide/html_single/Hibernate_User_Guide.html#bootstrap-native-metadata">
+     *     Hibernate User's Guide 5.2
+     *     </a>
+     * @param hibernateConfigurationFile : {@literal String} that
+     *                                   simply provides configuration
+     *                                   file name. It will be used in
+     *                                   for testing purposes. Default
+     *                                   is "hibernate.cfg.xml"
+     * @return {@literal SessionFactory} with which we can
+     * open sessions and make transactions
+     */
+    private SessionFactory
+    buildSessionFactory(String hibernateConfigurationFile) {
+        final ServiceRegistry serviceRegistry =
+                new StandardServiceRegistryBuilder().configure(
+                        hibernateConfigurationFile
+                ).build();
+        return new MetadataSources(serviceRegistry)
+                .buildMetadata()
+                .buildSessionFactory();
+    }
+
+    // constructors
+
+    // this kind of constructor is especially helpful in
+    // Unit Testing, because by changing config we can
+    // easily switch databases
+    public ContactDaoImpl(String hibernateConfigurationFile) {
+        this.sessionFactory = buildSessionFactory(
+                hibernateConfigurationFile
+        );
+    }
+
     @Override
     public List<Contact> findAll() {
         return null;
